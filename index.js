514baf5a9c91abbcb9598cb18bc77b402ee38966
Coder Gautam <66850133+codergautam@users.noreply.github.com>
2022-12-10 02:31:01 +0000

    changes

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const fs = require("fs");
 const { Client, IntentsBitField, PermissionFlagsBits } = require('discord.js');
 
 const client = new Discord.Client({
-	intents: [IntentsBitField.Flags.GuildMembers, IntentsBitField.Flags.Guilds]
+  intents: [IntentsBitField.Flags.GuildMembers, IntentsBitField.Flags.Guilds]
 });
 
 const channels = require("./channels.js")
@@ -22,128 +22,128 @@ const users = require('./users.json');
 names = Object.keys(users);
 var aocToDisc = {};
 for (var i = 0; i < names.length; i++) {
-	let aoc = users[names[i]]["What is your Advent of Code Username? (Make sure you are logged in to see it!)"];
-	let disc = users[names[i]]["If you are participating in the RCC Discord server, you will be automatically added to specific channels when you complete stars. You can join here: https://discord.gg/hsN92V4  - Please enter your Discord username so we can verify you."]
-	if (disc == '') continue;
-	else if (!disc.includes("#")) aocToDisc[aoc] = disc.trim()
-	else aocToDisc[aoc] = disc.split("#")[0].trim()
+  let aoc = users[names[i]]["What is your Advent of Code Username? (Make sure you are logged in to see it!)"];
+  let disc = users[names[i]]["If you are participating in the RCC Discord server, you will be automatically added to specific channels when you complete stars. You can join here: https://discord.gg/hsN92V4  - Please enter your Discord username so we can verify you."]
+  if (disc == '') continue;
+  else if (!disc.includes("#")) aocToDisc[aoc] = disc.trim()
+  else aocToDisc[aoc] = disc.split("#")[0].trim()
 }
 
 const refresh = async () => {
-	await queryApi().then((t) => {
-		leaderboard = t;
-		return t;
-	})
+  await queryApi().then((t) => {
+    leaderboard = t;
+    return t;
+  })
 }
 function fullRefresh() {
-	refresh().then(async _ => {
-		const completedDays = {};
-		const aocData = leaderboard["members"];
-		const aocIds = Object.keys(aocData);
-		for (var i = 0; i < aocIds.length; i++) {
-			const n = Object.keys(aocData[aocIds[i]].completion_day_level);
-			const username = aocToDisc[aocData[aocIds[i]].name];
-			if (username) completedDays[username] = n;
-		}
+  refresh().then(async _ => {
+    const completedDays = {};
+    const aocData = leaderboard["members"];
+    const aocIds = Object.keys(aocData);
+    for (var i = 0; i < aocIds.length; i++) {
+      const n = Object.keys(aocData[aocIds[i]].completion_day_level);
+      const username = aocToDisc[aocData[aocIds[i]].name];
+      if (username) completedDays[username] = n;
+    }
 
-		const guild = client.guilds.cache.get(creds.ROCHESTER_GUILD);
-		const me = await guild.members.fetchMe();
-		if (me.permissions.has(PermissionFlagsBits.ManageGuild)) {
-			console.log("I have the Permission Manage Channels");
-			console.log(me.permissions.toArray())
+    const guild = client.guilds.cache.get(creds.ROCHESTER_GUILD);
+    const me = await guild.members.fetchMe();
+    if (me.permissions.has(PermissionFlagsBits.ManageGuild)) {
+      console.log("I have the Permission Manage Channels");
+      console.log(me.permissions.toArray())
 
-		} else {
-			console.log("I don't have Permission Manage Channels");
-			console.log(me.permissions.toArray())
-		}
-
-
-		const out = JSON.parse(fs.readFileSync("./out.json"))
-		Object.keys(completedDays).forEach((user) => {
-			let id = out[user.toLowerCase()]
-			if (!id) return console.log("couldnt find " + user);
-
-			// let days = completedDays[user];
-			// let days array of numbers from 1 to 25
-			// let days = new Array(6).fill(0).map((_, i) => i + 1);
-			let days = [1];
+    } else {
+      console.log("I don't have Permission Manage Channels");
+      console.log(me.permissions.toArray())
+    }
 
 
+    const out = JSON.parse(fs.readFileSync("./out.json"))
+    Object.keys(completedDays).forEach((user) => {
+      let id = out[user.toLowerCase()]
+      if (!id) return console.log("couldnt find " + user);
 
-			days.forEach(async (completed) => {
-				// console.log(channels[completed])
+      // let days = completedDays[user];
+      // let days array of numbers from 1 to 25
+      // let days = new Array(6).fill(0).map((_, i) => i + 1);
+      let days = [1];
 
-				const guild = client.guilds.resolve(creds.ROCHESTER_GUILD);
-				const channelss = await guild.channels.fetch();
-				let channel = [...channelss.values()].find(c => c.id == channels[completed]);
-				if (channel) {
-					const id = user.id;
 
-					if (!channel.permissionOverwrites.resolve(id)) {
 
-						// duser is discord user
-						// hmm how can we get user obj
+      days.forEach(async (completed) => {
+        // console.log(channels[completed])
 
-						//                 channel.permissionOverwrites.create(id, {
-						//                   SEND_MESSAGES: true,
-						//                   VIEW_CHANNEL: true
-						//                 });
-						// console.log("added "+user+" to "+completed)
-						//                 channel.send("added "+user+"!")
+        const guild = client.guilds.resolve(creds.ROCHESTER_GUILD);
+        const channelss = await guild.channels.fetch();
+        let channel = [...channelss.values()].find(c => c.id == channels[completed]);
+        if (channel) {
+          const id = user.id;
 
-					} else {
-						//already is in channel
-					}
+          if (!channel.permissionOverwrites.resolve(id)) {
 
-					for (value of channel.permissionOverwrites.cache.values()) {
-						// await channel.permissionOverwrites.cache.get(key).delete();
-						if (value.allow.has(PermissionFlagsBits.ViewChannel)) {
-							await channel.permissionOverwrites.edit(value.id, {
-								SendMessages: false,
-								ViewChannel: false
-							});
-						}
-					}
+            // duser is discord user
+            // hmm how can we get user obj
 
-				} else {
-					// already removed
-					console.log("already removed")
-				}
+            //                 channel.permissionOverwrites.create(id, {
+            //                   SEND_MESSAGES: true,
+            //                   VIEW_CHANNEL: true
+            //                 });
+            // console.log("added "+user+" to "+completed)
+            //                 channel.send("added "+user+"!")
 
-			})
+          } else {
+            //already is in channel
+          }
 
-		})
-	})
+          for (value of channel.permissionOverwrites.cache.values()) {
+            // await channel.permissionOverwrites.cache.get(key).delete();
+            if (value.allow.has(PermissionFlagsBits.ViewChannel)) {
+              await channel.permissionOverwrites.edit(value.id, {
+                SendMessages: false,
+                ViewChannel: false
+              });
+            }
+          }
+
+        } else {
+          // already removed
+          console.log("already removed")
+        }
+
+      })
+
+    })
+  })
 };
 
 setInterval(() => {
-	fullRefresh();
+  fullRefresh();
 }, 60000)
 // When the client is ready, run this code (only once)
 
 client.once('ready', async () => {
 
-	console.log('Ready!');
+  console.log('Ready!');
 
-	async function discordFetch() {
-		const guild = client.guilds.resolve(creds.ROCHESTER_GUILD);
-		const members = await guild.members.fetch();
-		let savejson = {}
-		members.forEach((user) => {
-			savejson[user.user.username.toLowerCase()] = user.id;
-		})
-		fs.writeFile('./out.json', JSON.stringify(savejson), function(err) {
-			if (err) throw err;
-			console.log('complete');
-		}
-		);
-	}
+  async function discordFetch() {
+    const guild = client.guilds.resolve(creds.ROCHESTER_GUILD);
+    const members = await guild.members.fetch();
+    let savejson = {}
+    members.forEach((user) => {
+      savejson[user.user.username.toLowerCase()] = user.id;
+    })
+    fs.writeFile('./out.json', JSON.stringify(savejson), function(err) {
+      if (err) throw err;
+      console.log('complete');
+    }
+    );
+  }
 
-	setInterval(() => {
-		discordFetch();
-	}, 1800000)
-	await discordFetch();
-	fullRefresh();
+  setInterval(() => {
+    discordFetch();
+  }, 1800000)
+  await discordFetch();
+  fullRefresh();
 });
 
 // client
