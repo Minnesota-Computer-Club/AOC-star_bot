343b42b83271a0011a174790508d6656fcdcb4d3
Junhao Jerry Zhang <jerry_mountain@hotmail.com>
2022-12-10 02:24:20 +0000

    formatted code

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 require("isomorphic-fetch")
+const creds = require('./.config/creds.json');
 
 const token = process.env['TOKEN'];
 const Discord = require('discord.js');
@@ -7,7 +8,7 @@ const fs = require("fs");
 const { Client, IntentsBitField } = require('discord.js');
 
 const client = new Discord.Client({
-  intents: [IntentsBitField.Flags.GuildMembers, IntentsBitField.Flags.Guilds]
+	intents: [IntentsBitField.Flags.GuildMembers, IntentsBitField.Flags.Guilds,]
 });
 
 const channels = require("./channels.js")
@@ -15,29 +16,109 @@ let queryApi = require("./queryApi")
 
 let leaderboard = {};
 
-function refresh() {
-  queryApi().then((t) => {
-    console.log(t)
-    leaderboard = t;
-  })
+
+const users = require('./users.json');
+
+names = Object.keys(users);
+var aocToDisc = {};
+for (var i = 0; i < names.length; i++) {
+	let aoc = users[names[i]]["What is your Advent of Code Username? (Make sure you are logged in to see it!)"];
+	let disc = users[names[i]]["If you are participating in the RCC Discord server, you will be automatically added to specific channels when you complete stars. You can join here: https://discord.gg/hsN92V4  - Please enter your Discord username so we can verify you."]
+	if (disc == '') continue;
+	else if (!disc.includes("#")) aocToDisc[aoc] = disc.trim()
+	else aocToDisc[aoc] = disc.split("#")[0].trim()
 }
 
+const refresh = async () => {
+	await queryApi().then((t) => {
+		leaderboard = t;
+		return t;
+	})
+}
+function fullRefresh() {
+	refresh().then(_ => {
+		const completedDays = {};
+		const aocData = leaderboard["members"];
+		const aocIds = Object.keys(aocData);
+		for (var i = 0; i < aocIds.length; i++) {
+			const n = Object.keys(aocData[aocIds[i]].completion_day_level);
+			const username = aocToDisc[aocData[aocIds[i]].name];
+			if (username) completedDays[username] = n;
+		}
+		const out = JSON.parse(fs.readFileSync("./out.json"))
+		Object.keys(completedDays).forEach((user) => {
+			let id = out[user.toLowerCase()]
+			if (!id) return console.log("couldnt find " + user);
+
+			let days = completedDays[user];
+
+
+			days.forEach(async (completed) => {
+				console.log(channels[completed])
+
+				const guild = client.guilds.resolve(creds.ROCHESTER_GUILD);
+				const channelss = await guild.channels.fetch();
+				let channel = [...channelss.values()].find(c => c.id == channels[completed]);
+				if (channel) {
+					const id = user.id;
+
+					if (!channel.permissionOverwrites.resolve(id)) {
+
+						// duser is discord user
+						// hmm how can we get user obj
+
+						//                 channel.permissionOverwrites.create(id, {
+						//                   SEND_MESSAGES: true,
+						//                   VIEW_CHANNEL: true
+						//                 });
+						// console.log("added "+user+" to "+completed)
+						//                 channel.send("added "+user+"!")
+
+					} else {
+						//already is in channel
+					}
+
+					await channel.permissionOverwrites.create(channel.guild.roles.everyone, { ViewChannel: false })
+					console.log("channel " + channel.name)
+				} else console.log("channel " + completed + " not found")
+			})
+		})
+	})
+};
+
+setInterval(() => {
+	fullRefresh();
+}, 60000)
 // When the client is ready, run this code (only once)
-client.once('ready', () => {
 
-  console.log('Ready!');
+client.once('ready', async () => {
 
-  client.user.setActivity(`you solve aoc`, {
-    type: 'WATCHING'
-  });
+	console.log('Ready!');
 
+	async function discordFetch() {
+		const guild = client.guilds.resolve(creds.ROCHESTER_GUILD);
+		const members = await guild.members.fetch();
+		let savejson = {}
+		members.forEach((user) => {
+			savejson[user.user.username.toLowerCase()] = user.id;
+		})
+		fs.writeFile('./out.json', JSON.stringify(savejson), function(err) {
+			if (err) throw err;
+			console.log('complete');
+		}
+		);
+	}
 
-  setInterval(() => {
-    refresh()
-
-  }, 900000)
-  refresh()
+	setInterval(() => {
+		discordFetch();
+	}, 1800000)
+	await discordFetch();
+	fullRefresh();
 });
+
+// client
+//     .on("debug", console.log)
+//     .on("warn", console.log)
 
 // Login to Discord with your client's token
 client.login(token);
